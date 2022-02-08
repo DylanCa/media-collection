999ca2324ef128b50e1b6ad1358bc6ef308248e4
Dylan Cattelan <dylan.cattelan@gmail.com>
2022-02-08 13:29:38 +0100

    Added Season Serializer

--- collection/shows/api/serializers.py
+++ collection/shows/api/serializers.py
@@ -14,3 +14,21 @@ class EpisodeSerializer(serializers.ModelSerializer):
         if not self.instance:
             attrs["season_id"] = self.context.get("view").kwargs["season_pk"]
         return attrs
+
+
+class SeasonSerializer(serializers.ModelSerializer):
+    episodes = EpisodeSerializer(many=True)
+
+    class Meta:
+        model = Season
+        fields = (
+            "season_number",
+            "episodes",
+        )
+        read_only_fields = ("episodes",)
+
+    def validate(self, attrs):
+        if not self.instance:
+            attrs["show_id"] = self.context.get("view").kwargs["show_pk"]
+        return attrs
+

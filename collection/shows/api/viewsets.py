ee7c3200119e626633943d05723549ecd7143e70
Dylan Cattelan <dylan.cattelan@gmail.com>
2022-02-08 13:30:17 +0100

    Added Episode ViewSet

--- collection/shows/api/viewsets.py
+++ collection/shows/api/viewsets.py
@@ -54,3 +54,47 @@ class SeasonViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
                 status=status.HTTP_404_NOT_FOUND,
             )
 
+
+class EpisodeViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
+    serializer_class = EpisodeSerializer
+    queryset = Episode.objects.all()
+    resource_name = 'episodes'
+    permission_classes = ()
+    lookup_kwargs = {"pk": "id"}
+    parent_lookup_kwargs = {"season_pk": "season_id"}
+
+    def get_parent_object(self):
+        return Season.objects.get(id=self.kwargs["season_pk"])
+
+    def get_object(self):
+        return Episode.objects.get(id=self.kwargs["pk"])
+
+    def list(self, request, *args, **kwargs):
+        try:
+            season = self.get_parent_object()
+        except (Season.DoesNotExist):
+            return response.Response(
+                {"not_found": "Season not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        queryset = self.queryset.filter(season_id=season.id)
+        serializer = self.get_serializer(queryset, many=True)
+        return response.Response(serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        try:
+            season = self.get_parent_object()
+            episode = self.get_object()
+        except (Season.DoesNotExist, Episode.DoesNotExist):
+            return response.Response(
+                {"not_found": "Season or Episode not found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        if episode.season == season:
+            return super().retrieve(request, *args, **kwargs)
+        else:
+            return response.Response(
+                {"not_found": "Episode not found in this Season."},
+                status=status.HTTP_404_NOT_FOUND,
+            )

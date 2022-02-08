12e7ce52e8251bf52297bc4a4b901a57ac100eb1
Dylan Cattelan <dylan.cattelan@gmail.com>
2022-02-08 23:21:22 +0100

    Added Admin views to models

--- collection/shows/admin.py
+++ collection/shows/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Show, Season, Episode
 
 # Register your models here.
+class ShowAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "description")
+
+
+class SeasonAdmin(admin.ModelAdmin):
+    list_display = ("id", "show", "season_number")
+
+
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ("id", "season", "episode_number", "name")
+
+
+admin.site.register(Show, ShowAdmin)
+admin.site.register(Season, SeasonAdmin)
+admin.site.register(Episode, EpisodeAdmin)

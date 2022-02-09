155f744216649c4b988ac21410f651791d5cb861
Dylan Cattelan <dylan.cattelan@gmail.com>
2022-02-09 17:00:05 +0100

    Added several filters inside FilterViewsetMixin

--- collection/utils/filters.py
+++ collection/utils/filters.py
@@ -3,3 +3,23 @@ from django_filters.rest_framework import DjangoFilterBackend
 class FilterViewSetMixin():
     filter_backends = (DjangoFilterBackend,)
 
+    def filter_queryset(self, queryset):
+        queryset = super().filter_queryset(queryset)
+
+        title = self.request.query_params.get('title', None)
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+
+        name = self.request.query_params.get('name', None)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        resource_type = self.request.query_params.get('resource_type', None)
+        if resource_type:
+            queryset = queryset.filter(resource_type__model__icontains=resource_type)
+        
+        username = self.request.query_params.get('username', None)
+        if username:
+            queryset = queryset.filter(username__icontains=username)
+
+        return queryset
